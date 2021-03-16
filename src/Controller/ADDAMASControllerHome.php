a3f2178eb7575fccee29726b6934ebe7e64c2a83
Julien Préau <julienpreau@free.fr>
2021-03-16 14:35:20 +0100

    Maj : navbar et onglets fonctionnels

--- src/Controller/ADDAMASControllerHome.php
+++ src/Controller/ADDAMASControllerHome.php
@@ -14,4 +14,28 @@ class ADDAMASControllerHome extends Controller
     {
         return $this->render('home/home.html.twig');
     }
+
+    /**
+     * @Route("historique")
+     */
+    public function historique()
+    {
+        return $this->render('home/historique.html.twig');
+    }
+
+    /**
+     * @Route("produits")
+     */
+    public function produits()
+    {
+        return $this->render('home/produits.html.twig');
+    }
+
+    /**
+     * @Route("espace")
+     */
+    public function espace()
+    {
+        return $this->render('home/espace.html.twig');
+    }
 }
