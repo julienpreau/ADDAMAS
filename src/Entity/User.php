1c79b99470fabf6fed394811e6540b4f81c37377
Julien Préau <julienpreau@free.fr>
2021-03-18 09:06:53 +0100

    Maj : MAJ FOSuserBundle

---
+++ src/Entity/User.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use FOS\UserBundle\Model\User as BaseUser;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="'user'")
+ */
+class User extends BaseUser
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * @ORM\Column(type="integer")
+     */
+    protected $id;
+}
